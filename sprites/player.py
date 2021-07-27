995bcafe438ca663613e6d055dc2b8ff3454f54e
sweetkubuni <justsweng@gmail.com>
2021-07-26 22:25:00 -0400

    add jumping mechanic

--- sprites/player.py
+++ sprites/player.py
@@ -12,27 +12,44 @@ class Player(pygame.sprite.Sprite):
         self.rect.x, self.rect.y = x, y
         self.x, self.y = float(x), float(y) # xL: For more presice movement, we use floats instead of rect's int
         self.speed_x, self.speed_y = 0, 0 # xL: more floats
+        self.grounded = False
+        self.jump_height = 80;
+        self.gravity  = 10
+        self.jumping = False
+        self.jump_delay = -1;
+        self.start_height = 0;
     def update(self):
         # xL: Player movement controls
-        self.speed_x, self.speed_y = 0, 0
-        
+        self.speed_x = 0
         if self.game.actions[pygame.K_RIGHT]:
             self.speed_x += 1
         if self.game.actions[pygame.K_LEFT]:
             self.speed_x += -1
-        if self.game.actions[pygame.K_UP]:
-            self.speed_y += -1
+        if self.game.actions[pygame.K_UP] and self.grounded:
+            if self.jump_delay < 0:
+                self.jumping = True
+                self.grounded = False
+                self.jump_delay =  60
+                self.start_height = self.y
+
         if self.game.actions[pygame.K_DOWN]:
             self.speed_y += 1
         # xL: Player's jump botton
         if self.game.actions[pygame.K_z]:
             pass
 
+        self.jump_delay -= self.game.delta_time
+        if self.jumping:
+            self.speed_y = -100/30
+            if (self.start_height - self.y) >= self.jump_height:
+                self.jumping = False
+        else:
+            self.speed_y += self.gravity /30
+
         # xL: Applies the speed to the position
         self.x += self.speed_x * self.game.delta_time
         self.y += self.speed_y * self.game.delta_time
-        
-        # xL: Check collisions horizontally and then vertically
+
         self.rect.x = int(self.x)
         hit_list = pygame.sprite.spritecollide(self, self.game.state_stack[-1].current_level.tiles, False)
 
@@ -44,10 +61,10 @@ class Player(pygame.sprite.Sprite):
                 self.rect.left = tile.rect.right
                 self.speed_x = 0
             self.x = self.rect.x
-            
+
         self.rect.y = int(self.y)
         hit_list = pygame.sprite.spritecollide(self, self.game.state_stack[-1].current_level.tiles, False)
-        
+
         for tile in hit_list:
             if self.speed_y > 0:
                 self.rect.bottom = tile.rect.top
@@ -56,6 +73,7 @@ class Player(pygame.sprite.Sprite):
                 self.rect.top = tile.rect.bottom
                 self.speed_y = 0
             self.y = self.rect.y
+            self.grounded = True
 
     def draw(self):
         self.game.game_canvas.blit(self.image, self.rect)
